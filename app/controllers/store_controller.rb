d6b2adca9995c1154551d9b14e74d1ef919b52ed
deanvlue <munoz.josecarlos@gmail.com>
2014-10-29 06:27:44 -0600

    Starting functional testing

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,9 +1,6 @@
 class StoreController < ApplicationController
   def index
-<<<<<<< HEAD
     @products=Product.order(:title) 
-=======
     @products = Product.order(:title)
->>>>>>> 708923a163fa30507ffab6218f8c6b284f19b137
   end
 end
