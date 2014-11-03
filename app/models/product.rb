5ae634105c7f3338203746f21d649f2580c4b898
deanvlue-box <munoz.josecarlos@gmail.com>
2014-11-03 17:08:13 -0600

    Iteration D2 page 111

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,16 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$i},
     message: 'Debe ser una URL para imagen GIF, JPG o PNG'
   }
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base,'Line items present')
+      return false
+    end
+  end
 end
